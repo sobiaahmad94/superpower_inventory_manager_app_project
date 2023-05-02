f35a4eba076e0472e1c0f48248374a74ae613bb3
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-05-02 16:32:50 +0100

    superpower_products inventory table updated, stock conditions given

--- console.py
+++ console.py
@@ -17,16 +17,21 @@ manufacturer_repository.save(manufacturer_2)
 manufacturer_3 = Manufacturer("Shadow-Power Enterprise", "Be invisible by sinking into your shadow.", "shadow.power_support@shadow_power_enterprise.com", "Forest Hills, Queens, NY, United States")
 manufacturer_repository.save(manufacturer_3)
 
-
+manufacturer_4 = Manufacturer("Asteroid Eyes Corporation", "Wear contact lenses that give you 20/20 vision and also lets you gaze at the stars as though they're right beside you.", "asteroid_eyes_support@asteroid_eyes_corp.com", "Dublin, Ireland")
+manufacturer_repository.save(manufacturer_4)
 
 superpower_product_1 = SuperpowerProduct("Sapphire Glow", "This flower allows you to glow in the dark as you fly.", 70, 12.0, 30.0, manufacturer_1)
 superpower_product_repository.save(superpower_product_1)
 
-superpower_product_2 = SuperpowerProduct("Violet Flow Magnet", "This magnet makes you sing pitch-perfectly so you can sing with the birds or with Beyonce.", 50, 10.0, 20.0, manufacturer_2)
+superpower_product_2 = SuperpowerProduct("Violet Flow Magnet", "This magnet makes you sing pitch-perfectly so you can sing with the birds or with Beyonce.", 3, 10.0, 20.0, manufacturer_2)
 superpower_product_repository.save(superpower_product_2)
 
-superpower_product_3 = SuperpowerProduct("Rosy Cheeky Juice", "This shadow catcher drink enables you to be more confident - effects last for up to 48 hours.", 100, 14.0, 35.0, manufacturer_3)
+superpower_product_3 = SuperpowerProduct("Rosy Cheeky Juice", "This shadow catcher drink enables you to be more confident and have super speed - effects last for up to 48 hours.", 100, 14.0, 35.0, manufacturer_3)
 superpower_product_repository.save(superpower_product_3)
+
+superpower_product_4 = SuperpowerProduct("Asteroids Floating Contact Lenses", "These contact lenses let you see the stratosphere and the moon whilst teleporting there!", 0, 30.0, 70.0, manufacturer_4)
+superpower_product_repository.save(superpower_product_4)
+
 
 # Checking that superpower_product select_all() function is working
 
@@ -34,7 +39,7 @@ superpower_product_repository.save(superpower_product_3)
 
 # for superpower_product in all:
 #     print(superpower_product.manufacturer.__dict__)
-    # print(superpower_product.__dict__)
+#     print(superpower_product.__dict__)
 
 
 
--- controllers/superpower_inventory_controller.py
+++ controllers/superpower_inventory_controller.py
@@ -7,9 +7,29 @@ import repositories.manufacturer_repository as manufacturer_repository
 
 superpower_products_blueprint = Blueprint('superpower_products', __name__)
 
-# superpower_products index
-@superpower_products_blueprint.route('/superpower_products')
+# READ
+# superpower_products inventory table shown - superpower_products index.jinja
+# will use GET
+@superpower_products_blueprint.route('/superpower_products') # superpower_products index.jinja
 def superpower_products():
     superpower_products = superpower_product_repository.select_all()
-    return render_template('superpower_products/index.jinja', all_superpower_products = superpower_products)
+    return render_template('superpower_products/index.jinja', superpower_products = superpower_products)
+
+# NEW
+# add a new superpower_product to the superpower_products inventory - superpower_products/new
+# will use GET
+@superpower_products_blueprint.route('/superpower_products/new')
+def new_superpower_product():
+    superpower_products = superpower_product_repository.select_all()
+    return render_template('superpower_products/add_superpower_product.jinja', superpower_products = superpower_products)
+
+# UPDATE (edit)
+# edit a superpower_product within the superpower_products inventory - superpower_products/<id>/edit
+# will use POST
+
+
+# DELETE
+# delete a superpower_product within the superpower_products inventory - superpower_products/<id>/delete
+# will use POST
+
 
